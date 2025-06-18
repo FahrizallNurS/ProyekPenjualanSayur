7436adf871d4b36a3d7ff2774e42cd4c87cb9f31
Adinug122 <adi315429@gmail.com>
2025-06-18 11:51:59 +0700

    Update database.php

--- database.php
+++ database.php
@@ -2,11 +2,11 @@
 
 class Database
 {
-    private $host = 'sql307.infinityfree.com';
-    private $name = 'if0_39237537_db_sayur';
-    private $username = 'if0_39237537';
-    private $password = 'ZjycmZ7r8KR692';
-    private $port = 3306;
+    private $host = '';
+    private $name = '';
+    private $username = '';
+    private $password = '';
+    private $port = ;
     private $con;
 
     public function connection()
